6231316d52f665de670672399fdf5d6a96c4c3a3
wliafe <914099435@qq.com>
2025-04-28 17:22:47 +0800

    优化Animator类

--- mltools/draw.py
+++ mltools/draw.py
@@ -3,31 +3,24 @@ from matplotlib import pyplot as plt
 
 class Animator:
     """在动画中绘制数据"""
-    def __init__(self, *, line_num, xlabel=None, ylabel=None, xlim=None, ylim=None, legend=None, fmts=('-', 'm--', 'g-.', 'r:')):
-        # 增量地绘制多条线
-        self.fig, self.axes = plt.subplots()
-        self.xlabel,self.ylabel,self.xlim,self.ylim,self.legend=xlabel,ylabel,xlim,ylim,legend
-        self.X,self.Y=[[] for _ in range(line_num)],[[] for _ in range(line_num)]
-        self.fmts=fmts
-        
+    def __init__(self, *, line_num, xlabel=None, ylabel=None, xlim=None, ylim=None, legend=None):
+        self.fig, self.axes = plt.subplots() # 生成画布
+        self.set_axes= lambda : self.axes.set(xlabel=xlabel,ylabel=ylabel,xlim=xlim,ylim=ylim) # 初始化设置axes函数
+        self.legend=legend # 初始化标签
+        self.X,self.Y=[[] for _ in range(line_num)],[[] for _ in range(line_num)] # 初始化数据容器
     def add(self, *y):
+        '''添加数据'''
         for index,item in enumerate(y):
             self.Y[index].append(item)
             self.X[index].append(len(self.Y[index]))
-        self.axes.cla()
-        for index,(x,y) in enumerate(zip(self.X,self.Y)):
-            self.axes.plot(x,y,self.fmts[index])
-        self.set_axes()
-        display.display(self.fig)
-        display.clear_output(wait=True)
-
-    def set_axes(self):
-        self.axes.set_xlabel(self.xlabel)
-        self.axes.set_ylabel(self.ylabel)
-        self.axes.set_xlim(self.xlim)
-        self.axes.set_ylim(self.ylim)
-        if self.legend:self.axes.legend(self.legend)
-        self.axes.grid()
+        self.axes.cla() # 清除画布
+        for x,y,fmt in zip(self.X,self.Y,('-', 'm--', 'g-.', 'r:')):
+            self.axes.plot(x,y,fmt)
+        self.set_axes() # 设置axes
+        if self.legend:self.axes.legend(self.legend) # 设置标签
+        self.axes.grid() # 设置网格线
+        display.display(self.fig) # 画图
+        display.clear_output(wait=True) # 清除输出
 
 def images(images,labels,shape):
     '''展示图片'''
