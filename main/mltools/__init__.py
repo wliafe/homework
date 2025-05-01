3e2d77af7c826de999ba9bc0be4d9567fa94e961
wliafe <914099435@qq.com>
2025-05-01 11:10:12 +0800

    添加累加器和计时器

--- main/mltools/__init__.py
+++ main/mltools/__init__.py
@@ -1,2 +1,4 @@
 from .vocab import Vocab
 from .ml import MachineLearning
+from .accumulator import Accumulator
+from .timer import Timer
--- main/mltools/ml.py
+++ main/mltools/ml.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn,optim
-from . import draw
+from .draw import Animator
+from .timer import Timer
+from .accumulator import Accumulator
 
 class MachineLearning:
     def __init__(self, model, train_iter, val_iter=None, test_iter=None, *, device=torch.device('cpu')):
@@ -8,14 +10,19 @@ class MachineLearning:
         model.to(device) # 将网络复制到device上
         self.model, self.train_iter, self.val_iter, self.device = model, train_iter, val_iter, device
         self.test_iter = test_iter if test_iter else val_iter # 定义测试集
+        self.setTimer() # 设置计时器
         self.setLoss() # 设置损失函数
 
     def train(self,num_epochs,learning_rate):
         '''训练模型'''
         self.setOptimizer(learning_rate) # 定义优化器
         self.setAnimator(num_epochs) # 设置Animator
-        for _ in range(num_epochs):
-            self.train_epoch()
+        for num_epoch in range(1,num_epochs+1):
+            self.train_epoch(num_epoch)
+
+    def setTimer(self):
+        '''设置计时器'''
+        self.timer = Timer()
 
     def setLoss(self, loss=None):
         '''设置损失函数'''
@@ -27,12 +34,16 @@ class MachineLearning:
 
     def setAnimator(self, num_epochs):
         '''设置Animator'''
-        self.animator = draw.Animator(line_num=2,xlabel='epoch',ylabel='loss',xlim=[0, num_epochs+1],ylim=-0.1,legend=['train loss','val loss'])
+        self.animator = Animator(line_num=2,xlabel='epoch',ylabel='loss',xlim=[0, num_epochs+1],ylim=-0.1,legend=['train loss','val loss'])
 
-    def train_epoch(self):
+    def train_epoch(self, num_epoch):
         '''一个迭代周期'''
+        self.timer.start()
         train_loss=self.calculate_train_iter() # 计算训练集            
+        self.timer.stop()
         val_loss=self.calculate_val_iter() # 计算验证集            
+        print(f'train loss {train_loss:.3f}, val loss {val_loss:.3f}')
+        print(f'{self.timer.sum() / num_epoch:.1f} sec/epoch on {str(self.device)}')
         self.animator.add(train_loss.detach().cpu(),val_loss.detach().cpu()) # 添加损失值
 
     def calculate_train_iter(self):
@@ -68,15 +79,15 @@ class MachineLearning:
 
     def test(self):
         '''测试模型'''
-        test_num,real_num=0,0 # 定义测试数量和预测真实数量
+        accumulate=Accumulator(2) # 定义测试数量和预测真实数量
         # 测试
         for x,y in self.test_iter:
             pred=self.calculate_acc(x, y)
-            test_num+=len(pred)
-            real_num+=pred.sum()
-        print(f'Accuracy rate {real_num/test_num}') # 计算测试准确率并输出
+            accumulate.add(pred.sum(),len(pred))
+        print(f'Accuracy rate {accumulate[0] / accumulate[1]}') # 计算测试准确率并输出
 
     def calculate_acc(self, x, y):
+        '''计算准确率'''
         y_test=self.calculate_y(x)            
         y_test=y_test.argmax(dim=1)            
         y = y.to(self.device)            
