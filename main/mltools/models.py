fd5d96eed721c5841f6a2173e34c048a637182a7
wliafe <914099435@qq.com>
2025-05-02 11:03:06 +0800

    models 修改

--- main/mltools/models.py
+++ main/mltools/models.py
@@ -7,13 +7,13 @@ class RNNModel(nn.Module):
         super(RNNModel, self).__init__(**kwargs)
         self.rnn, self.vocab_size, self.step_size, self.one_hot = rnn_layer, vocab_size, step_size, one_hot
         self.output_size = output_size if output_size else vocab_size
-        self.num_hiddens = self.rnn.hidden_size
-        self.num_directions = 1 if not self.rnn.bidirectional else 2 # 如果RNN是双向的，num_directions应该是2，否则应该是1
-        self.linear = nn.Linear(self.step_size * self.num_hiddens * self.num_directions, self.output_size) # 定义输出层
+        self.hidden_size = self.rnn.hidden_size
+        self.directions = 1 if not self.rnn.bidirectional else 2 # 如果RNN是双向的，num_directions应该是2，否则应该是1
+        self.linear = nn.Linear(self.step_size * self.hidden_size * self.directions, self.output_size) # 定义输出层
 
     def forward(self, inputs):
         X = F.one_hot(inputs.long(), self.vocab_size) if self.one_hot else inputs # 将输入独热编码，X形状为(批量大小, 时间步数, 词表大小)
         X = X.to(torch.float32)
         Y, state = self.rnn(X) # Y形状为(批量大小, 时间步数, 隐藏大小),state形状为(隐藏层数*num_directions, 批量大小, 隐藏大小)
-        output = self.linear(Y.reshape((-1, self.step_size * self.num_hiddens * self.num_directions))) # 它的输出形状是(批量大小, 输出大小)。
+        output = self.linear(Y.reshape(-1, self.step_size * self.hidden_size * self.directions)) # 它的输出形状是(批量大小, 输出大小)。
         return output, state
